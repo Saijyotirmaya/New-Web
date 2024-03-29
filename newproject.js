f2712831b3a9299b5f9ed53052739fddaeb45566
Sai j behera <93567575+Saijyotirmaya@users.noreply.github.com>
2024-03-29 12:34:34 +0530

    Create newproject.js
    for a smooth scroll the webpage

---
+++ newproject.js
@@ -0,0 +1,3 @@
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.3/gsap.min.js" integrity="sha512-7Au1ULjlT8PP1Ygs6mDZh9NuQD0A5prSrAUiPHMXpU6g3UMd8qesVnhug5X4RoDr35x5upNpx0A6Sisz1LSTXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+     <script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.min.js"></script>
+    <script src="loco.js"></script>
